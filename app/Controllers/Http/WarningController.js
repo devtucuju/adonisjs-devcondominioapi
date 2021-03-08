75f1ae974120d59797696a8862e645c08f0a626f
devtucuju <jsdeveloperweb@gmail.com>
2021-03-08 16:46:22 -0300

    :rocket: lista warnings do usuario

--- app/Controllers/Http/WarningController.js
+++ app/Controllers/Http/WarningController.js
@@ -1,6 +1,34 @@
 'use strict'
+const Warning = use('App/Models/Warning')
+const Unit = use('App/Models/Unit')
 
 class WarningController {
+  async show ({ request, response, auth }) {
+    const array = { error: '' }
+
+    const user = await auth.user
+    const property = request.input('property')
+
+    if (property) {
+      const unit = await Unit.query().where('id', property).where('user_id', user.id).getCount()
+
+      if (unit > 0) {
+        // ordenar pela data de criacao e id
+        const warnings = await Warning.query().where('unit_id', property).fetch()
+
+        // adicionar em cada warning o file url,formatar a data e criar um array com as urls da imagens
+        array.list = warnings
+      } else {
+        array.error = 'Está unidade não é sua!'
+      }
+    } else {
+      array.error = 'A propriedade é Obrigatoria!'
+    }
+
+    // serializar o array para trazer o id, titulo e url do arquivo
+
+    return response.json(array)
+  }
 }
 
 module.exports = WarningController
