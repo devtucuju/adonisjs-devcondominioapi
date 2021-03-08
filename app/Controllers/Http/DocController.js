8a4d4e6299cb4acce56f549cac897878f690898e
devtucuju <jsdeveloperweb@gmail.com>
2021-03-08 11:15:12 -0300

    :rocket: lista todos os documentos

--- app/Controllers/Http/DocController.js
+++ app/Controllers/Http/DocController.js
@@ -1,6 +1,21 @@
 'use strict'
+const Doc = use('App/Models/Doc')
 
 class DocController {
+  async index ({ request, response, auth }) {
+    const array = { error: '' }
+    const docs = await Doc.all()
+    array.list = docs
+
+    // serializar o array para trazer o id, titulo e url do arquivo
+    // const serialization = []
+
+    // for (const wall of docs.toJSON()) {
+
+    // }
+
+    return response.json(array)
+  }
 }
 
 module.exports = DocController
