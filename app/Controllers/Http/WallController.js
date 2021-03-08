512edf69a7ffc6667c7b42a43d3c4d01ceb2a1c5
devtucuju <jsdeveloperweb@gmail.com>
2021-03-08 08:30:26 -0300

    :rocket: adiciona metodo like

--- app/Controllers/Http/WallController.js
+++ app/Controllers/Http/WallController.js
@@ -19,6 +19,36 @@ class WallController {
 
     return response.json(serialization)
   }
+
+  async like ({ request, response, auth }) {
+    const array = { error: '' }
+
+    const { id } = request.params
+    const user = await auth.user
+    // verificar se o usuario logado ja deu like no quadro de avisos
+    const liked = await WallLike.query().where('wall_id', id).where('user_id', user.id).getCount()
+
+    if (liked > 0) {
+      // remover o like
+      await WallLike.query().where('wall_id', id).where('user_id', user.id).delete()
+      array.liked = false
+    } else {
+      // adiconar o like
+      const likeData = {
+        wall_id: id,
+        user_id: user.id
+      }
+      await WallLike.create(likeData)
+      array.liked = true
+    }
+
+    // recontar a quantidade de likes no mural e inserir na resposta
+
+    const likes = await WallLike.query().where('wall_id', id).getCount()
+    array.likes = likes
+
+    return response.json(array)
+  }
 }
 
 module.exports = WallController
